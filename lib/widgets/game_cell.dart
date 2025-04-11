5411686f43219b516f972d971ac455eab55a36a5
Глазырев Константин Сергеевич <k.glazyrev@mygiftcard.ru>
2025-04-11 16:29:12 +0300

    broke the game into blocks

---
+++ lib/widgets/game_cell.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
+
+import '../models/image_item.dart';
+import '../widgets/draggable_item.dart';
+
+class GameCell extends StatelessWidget {
+  final ImageItem? imageItem;
+  final Function onDrop;
+
+  const GameCell({super.key, this.imageItem, required this.onDrop});
+
+  @override
+  Widget build(BuildContext context) {
+    return DragTarget<ImageItem>(
+      onAccept: (imageItem) {
+        onDrop(imageItem);
+      },
+      builder: (context, candidateItems, rejectedItems) {
+        return Container(
+          width: 60,
+          height: 60,
+          color: Colors.grey[300],
+          child:
+              imageItem == null
+                  ? null
+                  : DraggableImage(
+                    imageItem: imageItem!,
+                    onDragEnd: (details) {
+                      // Handle onDragEnd (if needed)
+                    },
+                  ),
+        );
+      },
+    );
+  }
+}
+
+// class GameCell {
+//   final ImageItem? item;
+//   GameCell.empty() : item = null;
+//   GameCell.withItem(this.item);
+//   bool get isEmpty => item == null;
+// }
