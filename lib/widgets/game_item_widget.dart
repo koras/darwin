71a8f90dd96873ad8db079bb2c734b2aa7358b9c
Глазырев Константин Сергеевич <k.glazyrev@mygiftcard.ru>
2025-04-12 21:01:57 +0300

    Добавил название предмета

--- lib/widgets/game_item_widget.dart
+++ lib/widgets/game_item_widget.dart
@@ -14,21 +14,33 @@ class GameItemWidget extends StatelessWidget {
       left: item.gridX * cellSize + cellSize * 0.1,
       top: item.gridY * cellSize + cellSize * 0.1,
       child: GestureDetector(
-        child: Container(
-          width: cellSize * 0.8,
-          height: cellSize * 0.8,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                blurRadius: 4,
-                spreadRadius: 1,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: cellSize * 0.8,
+              height: cellSize * 0.8,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 4,
+                    spreadRadius: 1,
+                  ),
+                ],
               ),
-            ],
-          ),
 
-          child: Image.asset(item.assetPath, fit: BoxFit.contain),
+              child: Image.asset(item.assetPath, fit: BoxFit.contain),
+            ),
+            Text(
+              item.slug, // Предполагая, что в GameItem есть поле name
+              style: TextStyle(fontSize: 10, fontWeight: FontWeight.normal),
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
         ),
       ),
     );
