52980b696976b85c96f10b2a29243ec413daa31c
Глазырев Константин Сергеевич <k.glazyrev@mygiftcard.ru>
2025-05-18 12:49:49 +0300

    База для локализации. Приступаем к переводу

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,13 @@
+// Английские локализации для приложения
+// Содержит все тексты на английском языке
+class AppLocalizationsEn {
+  static const Map<String, String> texts = {
+    'gameTitle': 'Darwin Evolution',
+    'hintTitle': 'Hint',
+    'combinationsTitle': 'All Combinations',
+    'discoveredCombinations': 'Your discovered combinations:',
+    'allCombinations': 'All possible combinations:',
+    'continueWithoutHints': 'Continue without hints',
+    // Добавьте другие строки
+  };
+}
